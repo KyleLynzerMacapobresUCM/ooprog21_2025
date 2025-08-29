024937983352b761305b08a48a6caef738544783
unknown <Kyle Lynzer Macapobres>
2025-08-29 18:42:12 +0800

    final
    Signed-off-by: unknown <Kyle Lynzer Macapobres>

--- Chapter_1/TableAndChairs.java
+++ Chapter_1/TableAndChairs.java
@@ -7,6 +7,6 @@ public class TableAndChairs {
         System.out.println("X       XXXXXXXXXX        X");
         System.out.println("XXXXX   X        X    XXXXX");
         System.out.println("X   X   X        X    X   X");
-        System.out.println("X   X   X        X    X   X")
+        System.out.println("X   X   X        X    X   X");
     }
 }
