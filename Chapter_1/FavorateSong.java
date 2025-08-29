53c37bcb9433520f1133585de664d9c13f89eb4d
unknown <Kyle Lynzer Macapobres>
2025-08-29 18:46:21 +0800

    final
    Signed-off-by: unknown <Kyle Lynzer Macapobres>

---
+++ Chapter_1/FavorateSong.java
@@ -0,0 +1,10 @@
+package Chapter_1;
+
+public class FavorateSong {
+    public static void main(String[] args) {
+        System.out.println("I'm like a statue, stuck staring right at you");
+        System.out.println("Got me frozen in my tracks");
+        System.out.println("So amazed how you take me back");
+        System.out.println("Each and every time our love collapsed");
+    }
+}
