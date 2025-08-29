41102984116f34af4330cf32636d18cf6943c5fe
unknown <Kyle Lynzer Macapobres>
2025-08-29 18:50:49 +0800

    final
    Signed-off-by: unknown <Kyle Lynzer Macapobres>

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,11 @@
+package Chapter_1;
+
+public class Comments  
+{
+    public static void main(String[] args)
+    {
+        /* the mutli-line */
+        System.out.println("Hi, this is comment");
+         /** the documentation */
+    }
+}
