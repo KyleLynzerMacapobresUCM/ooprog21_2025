ba6378f190f477ca33596bc4b0c0c4b68f4feb10
unknown <Kyle Lynzer Macapobres>
2025-08-31 12:45:14 +0800

    final
    Signed-off-by: unknown <Kyle Lynzer Macapobres>

--- Chapter_1/FixDebugOne.java
+++ Chapter_1/FixDebugOne.java
@@ -2,18 +2,14 @@ package Chapter_1;
 
 public class FixDebugOne
 {
-   /* This program displays a greeting */
+
    public static void main(String[] args)   
    {
       System.out.println("Hello World!");
    }
 }
 /*
-List down the spotted bugs below:
-
-e.g.
-- // wrong syntax for access modifier "publlic" instead of "public"
-
+Listed spotted bugs below:
 Bugs:
 1. Class name has a space → `FixDebug One` should be `FixDebugOne`.
 2. Method name `Main` should be lowercase → `main`.
