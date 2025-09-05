0c4ebe74783330e7df3574d5ef81fde0e328a558
unknown <Kyle Lynzer Macapobres>
2025-09-05 16:03:15 +0800

    Signed-off-by: unknown <Kyle Lynzer Macapobres>

--- Chapter_2/UsingJOptionPaneDialogs.java
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -5,9 +5,41 @@ import javax.swing.JOptionPane;
 public class UsingJOptionPaneDialogs {
 
     public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("What is your name?");
-        
-        JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
-    }
+        while (true) {
+            
+            String name = JOptionPane.showInputDialog(
+                    null,
+                    "What is your name?",
+                    "Input",
+                    JOptionPane.QUESTION_MESSAGE
+            );
 
+            
+            if (name == null) {
+                JOptionPane.showMessageDialog(null, "Goodbye!");
+                System.exit(0);
+            }
+
+            
+            int choice = JOptionPane.showConfirmDialog(
+                    null,
+                    "Do you want your name to be shown?",
+                    "Confirmation",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE
+            );
+
+            
+            if (choice == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Hello, " + name + "!",
+                        "Message",
+                        JOptionPane.INFORMATION_MESSAGE
+                );
+                break; 
+            }
+             
+        }
+    }
 }
