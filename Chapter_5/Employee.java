271110fe05497f9b6fcb551fef82286c3b1faec8
Kyle Lynzer V. Macapobres <kylelynzermacapobres20@gmail.com>
2025-10-20 10:51:30 +0800

    Signed-off-by: unknown <Kyle Lynzer Macapobres>

--- Chapter_5/Employee.java
+++ Chapter_5/Employee.java
@@ -1,27 +1,27 @@
 package Chapter_5;
 
 public class Employee {
-    // Instance variable to store the employee's hourly pay rate
+ 
     double payRate;
 
-    // Constructor to initialize the pay rate when creating an Employee object
+   
     public Employee(double payRate) {
         this.payRate = payRate;
     }
 
-    // Method to calculate regular pay (for up to 40 hours)
+    
     public double getRegularPay(double hours) {
-        // Regular hours are capped at 40
+        
         double regularHours = Math.min(hours, 40);
-        // Return pay for regular hours
+        
         return regularHours * payRate;
     }
 
-    // Method to calculate overtime pay (for hours beyond 40)
+
     public double getOvertimePay(double hours) {
-        // Overtime hours are any hours over 40
+        
         double overtimeHours = Math.max(0, hours - 40);
-        // Overtime pay is 1.5 times the regular rate
+       
         return overtimeHours * payRate * 1.5;
     }
 }
